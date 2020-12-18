bee2ccc2f25a559902f414c5b52d0d8a5653706d
kcelinned <kc_diccion@yahoo.co.uk>
2020-12-18 21:05:56 +0000

    Tweepy Test

---
+++ main.py
@@ -0,0 +1,10 @@
+import tweepy
+import config
+
+auth = tweepy.AppAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
+api = tweepy.API(auth, wait_on_rate_limit = True) 
+
+collectTweets = api.search("Boris", 'en', count = 5, result_type = 'mixed')
+
+for tweet in collectTweets:
+    print(tweet.text )
